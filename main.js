0bb6dbaa3c9a51d012d143d51b6c82f6e44cc1f8
zecy <angelszc163@gmail.com>
2017-04-03 17:07:45 +0800

    改用导入方式

--- main.js
+++ main.js
@@ -1,37 +1,37 @@
 'use strict';
 
-// @flow
+
 const main = () => {
 
     // 定义插入按钮
-    const hasInfoBlock: null | HTMLElement = document.getElementById('info');
-    let infoBlock: HTMLElement;
+    const hasInfoBlock = document.getElementById('info');
+    let infoBlock;
     if (hasInfoBlock) {
         infoBlock = hasInfoBlock;
     } else {
         alert('\n\nnHentai 下载脚本错误：\n\n\n　　插入按钮出错，请查看页面源码是否进行了更新。\n\n');
-        return
+        return;
     }
 
-    let btnArea: HTMLCollection < HTMLElement > = infoBlock.getElementsByClassName('buttons');
+    let btnArea = infoBlock.getElementsByClassName('buttons');
 
-    const btn: string = '<button id="getimglist" type="button" class="btn btn-primary" onclick="insertList();">点击获取图片链接</button>';
+    const btn = '<button id="getimglist" type="button" class="btn btn-primary" onclick="insertList();">点击获取图片链接</button>';
     btnArea[0].innerHTML = btnArea[0].innerHTML + btn;
 
     // 获取链接列表
     const getImageLinks = () => {
         // 获取链接
-        const rawImgLinks: HTMLCollection < HTMLImageElement > = (document.querySelectorAll('.gallerythumb > img'): any);
-        const linkPattern: RegExp = /https:\/\/t.nhentai.net\/galleries\/(\d+)\/(\d+)t.(jpg|png)/;
+        const rawImgLinks = document.querySelectorAll('.gallerythumb > img');
+        const linkPattern = /https:\/\/t.nhentai.net\/galleries\/(\d+)\/(\d+)t.(jpg|png)/;
 
-        let imgLinks: Array < string > = Array.from(rawImgLinks).map((imgLink: HTMLImageElement) => {
-            let rawLink: string;
+        let imgLinks = Array.from(rawImgLinks).map(imgLink => {
+            let rawLink;
             if (!linkPattern.test(imgLink.src)) {
-                rawLink = imgLink.dataset.src
+                rawLink = imgLink.dataset.src;
             } else {
-                rawLink = imgLink.src
+                rawLink = imgLink.src;
             }
-            const link: string = rawLink.replace(linkPattern, "https://i.nhentai.net/galleries/$1/$2.$3");
+            const link = rawLink.replace(linkPattern, "https://i.nhentai.net/galleries/$1/$2.$3");
             return link;
         });
 
@@ -39,14 +39,14 @@ const main = () => {
     };
 
     // 获取链接列表
-    const imglist: string = getImageLinks().join('<br>');
+    const imglist = getImageLinks().join('<br>');
 
     // 链接注入当前网页
     const insertList = () => {
         // 列表框定位
-        //// 父元素
-        const hasContent: null | HTMLElement = document.getElementById('content');
-        let content: HTMLElement;
+
+        const hasContent = document.getElementById('content');
+        let content;
         if (hasContent) {
             content = hasContent;
         } else {
@@ -54,8 +54,8 @@ const main = () => {
             return;
         }
         //// 要插入到这个元素前面
-        const hasThumbnailContainer: null | HTMLElement = document.getElementById('thumbnail-container');
-        let thumbnailContainer: HTMLElement;
+        const hasThumbnailContainer = document.getElementById('thumbnail-container');
+        let thumbnailContainer;
         if (hasThumbnailContainer) {
             thumbnailContainer = hasThumbnailContainer;
         } else {
@@ -65,7 +65,7 @@ const main = () => {
 
         // 创建一个列表框
         if (!document.getElementById('listbox')) {
-            let listBox: HTMLDivElement = document.createElement('div');
+            let listBox = document.createElement('div');
             listBox.id = 'listbox';
             listBox.className = 'container';
 
@@ -76,7 +76,7 @@ const main = () => {
             listBox.innerHTML = '<div id="imglistbox" contentEditable>' + imglist + '</div>';
         } else {
             // 如果列表框已经存在
-            let imgListBox: null | HTMLElement = document.getElementById('imglistbox');
+            let imgListBox = document.getElementById('imglistbox');
             if (imgListBox) {
                 imgListBox.innerHTML = imglist;
             }
@@ -85,4 +85,4 @@ const main = () => {
     };
     // 函数注入页面
     window.insertList = insertList;
-}
+};
